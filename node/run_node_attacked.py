4cdf223f57e5611aaa3f04c81686c02c5cd162d5
Ivan Pérez <77869571+idperez720@users.noreply.github.com>
2022-02-22 20:31:22 -0500

    Update run_node_attacked.py

--- node/run_node_attacked.py
+++ node/run_node_attacked.py
@@ -6,6 +6,7 @@ from fsm.finite_state_machine_with_reconfiguration import *
 
 def main_atack():
     # Creates server socket and launches server
+    print("Attack began")
     communicator = Node_Comunicator()
     communicator.bind_server_socket()
     communicator.run_server()
